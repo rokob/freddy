41a818df65a8119542a2a964598c61ccd1153893
Andrew Ledvina <led@fb.com>
2014-02-17 18:24:24 -0500

    Added a view controller, nothing really happening yet, still messing around with cocoapods

---
+++ freddy/RKFeedNavigationController.h
@@ -0,0 +1,9 @@
+//  Copyright (c) 2014 rokob. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+#import <RKIntent/RKIntentHandler.h>
+
+@interface RKFeedNavigationController : UINavigationController //<RKIntentHandler>
+
+@end
---
+++ freddy/RKFeedNavigationController.m
@@ -0,0 +1,32 @@
+//  Copyright (c) 2014 rokob. All rights reserved.
+
+#import "RKFeedNavigationController.h"
+
+@interface RKFeedNavigationController ()
+
+@end
+
+@implementation RKFeedNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    // Custom initialization
+  }
+  return self;
+}
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
